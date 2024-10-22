e691884bbcfed53ce4ee6de40198d916b4fbd2a9
Anuprita Zarkar <112081492+anupritazarkar@users.noreply.github.com>
2024-10-22 10:03:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
